13ecbf4d0697b9cb89b2272ddf01b717ea9e734c
Dan Quan <dan@quan.io>
2025-11-11 19:50:59 -0800

    Fix tests

--- packages/shared/vitest.config.ts
+++ packages/shared/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    setupFiles: ['./src/test-setup.ts'],
+    setupFiles: [path.resolve(__dirname, './src/test-setup.ts')],
   },
 });
